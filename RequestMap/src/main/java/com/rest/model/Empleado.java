a04245c36aea46a382ec0ba75342f1fcbbf252f4
erickamontero <monteroh.ericka@gmail.com>
2019-09-24 20:32:10 -0500

    project request map

---
+++ RequestMap/src/main/java/com/rest/model/Empleado.java
@@ -0,0 +1,53 @@
+package com.rest.model;
+
+import java.util.Date;
+
+public class Empleado {
+	private long id;
+    private String nombre;
+    private String apellidos;
+    private Date fechaNacimiento;
+
+    public Empleado() {}
+
+
+    public Empleado(long id, String nombre, String apellidos, Date fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+}
