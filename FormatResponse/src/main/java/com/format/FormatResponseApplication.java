d084bfa5ecacf33769fb560379c9284a9ae03a9b
erickamontero <monteroh.ericka@gmail.com>
2019-09-24 21:02:12 -0500

    project format response

---
+++ FormatResponse/src/main/java/com/format/FormatResponseApplication.java
@@ -0,0 +1,13 @@
+package com.format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatResponseApplication.class, args);
+	}
+
+}
