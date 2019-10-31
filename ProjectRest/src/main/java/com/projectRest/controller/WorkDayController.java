bf8601a7b039de4eeea2ad4aa23a7ad59227282d
Ericka Montero <ezmonte@transunion.com>
2019-10-31 11:45:30 -0500

    adjustment exception not found

--- ProjectRest/src/main/java/com/projectRest/controller/WorkDayController.java
+++ ProjectRest/src/main/java/com/projectRest/controller/WorkDayController.java
@@ -2,6 +2,7 @@ package com.projectRest.controller;
 
 
 import com.projectRest.model.Workday;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -17,29 +18,34 @@ public class WorkDayController {
     @GetMapping("/workday")
     public List<Workday> getWorkday() {
 
-            return entityWorkdays;
+        return entityWorkdays;
 
     }
+
     @GetMapping("/workday/{id}")
     public Workday getWorkdayById(@PathVariable Long id) {
         if (id > 0 && id <= entityWorkdays.size()) {
             return entityWorkdays.get((int) (id - 1));
+        } else {
+            throw new WorkdayNotFoundException(id);
         }
 
-        return null;
+
     }
 
     @GetMapping("/workday/findname/{name}")
-    public Workday getWorkdayByName(@PathVariable  String name) {
-
-        for (Workday workday : entityWorkdays) {
-            if (workday.getName().equalsIgnoreCase(name)) {
-                return workday;
+    public Workday getWorkdayByName(@PathVariable String name) {
+        Workday workday = null;
+        for (Workday model : entityWorkdays) {
+            if (model.getName().equalsIgnoreCase(name)) {
+                workday = model;
             }
         }
-
-
-        return null;
+        if (workday != null) {
+            return workday;
+        } else {
+            throw new WorkdayNotFoundException(name);
+        }
     }
 
     @PostConstruct
@@ -49,6 +55,22 @@ public class WorkDayController {
         entityWorkdays = Arrays.asList(workday8H, workday12H);
 
     }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    private class WorkdayNotFoundException extends RuntimeException {
+
+        private static final long serialVersionUID = -5798132769496018860L;
+
+        public WorkdayNotFoundException(Long id) {
+            super(String.format("La jornada %d no existe", id));
+        }
+
+        public WorkdayNotFoundException(String name) {
+            super(String.format("La jornada %s no existe", name));
+        }
+
+    }
+
 }
 
 
